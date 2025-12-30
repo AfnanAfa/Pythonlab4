70e96cc57c35b675556b078a0198495fb9d06b10
AfnanAfa <aahrafafna2368@gmail.com>
2025-12-30 13:15:29 +0530

    graphics folder restructured

--- graphics/main.py
+++ graphics/main.py
@@ -1,8 +1,7 @@
-<<<<<<< HEAD
-from graphics.rectangle import area, perimeter
-from graphics.circle import area as c_area, perimeter as c_peri
-from graphics.ThreeD_graphics.cuboid import surface_area, perimeter as cub_peri
-from graphics.ThreeD_graphics.sphere import surface_area as s_area, perimeter as s_peri
+from TwoD_graphics.rectangle import area, perimeter
+from TwoD_graphics.circle import area as c_area, perimeter as c_peri
+from ThreeD_graphics.cuboid import surface_area, perimeter as cub_peri
+from ThreeD_graphics.sphere import surface_area as s_area, perimeter as s_peri
 
 print("Rectangle Area:", area(5, 4))
 print("Rectangle Perimeter:", perimeter(5, 4))
@@ -14,21 +13,3 @@ print("Cuboid Surface Area:", surface_area(2, 3, 4))
 print("Cuboid Perimeter:", cub_peri(2, 3, 4))
 
 print("Sphere Surface Area:", s_area(5))
-=======
-from graphics.rectangle import area, perimeter
-from graphics.circle import area as c_area, perimeter as c_peri
-from graphics.ThreeD_graphics.cuboid import surface_area, perimeter as cub_peri
-from graphics.ThreeD_graphics.sphere import surface_area as s_area, perimeter as s_peri
-
-print("Rectangle Area:", area(5, 4))
-print("Rectangle Perimeter:", perimeter(5, 4))
-
-print("Circle Area:", c_area(7))
-print("Circle Perimeter:", c_peri(7))
-
-print("Cuboid Surface Area:", surface_area(2, 3, 4))
-print("Cuboid Perimeter:", cub_peri(2, 3, 4))
-
-print("Sphere Surface Area:", s_area(5))
->>>>>>> 14068e2969c9088dd4640f2e3775836c863aa5c0
-print("Sphere Perimeter:", s_peri(5))
