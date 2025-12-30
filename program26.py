73e1ee4f31fd1f1b7f6d49740cc6f3cb79c08a77
AfnanAfa <aahrafafna2368@gmail.com>
2025-12-30 11:38:43 +0530

    updated all programs in python

---
+++ program26.py
@@ -0,0 +1,14 @@
+#Write a Python program to read specific columns of a given CSV file and print the content
+
+#of the columns.
+
+
+
+import csv
+
+filename = "data.csv"
+
+with open(filename, "r") as file:
+    reader = csv.reader(file)
+    for row in reader:
+        print(row[0], row[2])
